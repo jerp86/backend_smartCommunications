57471bb90f3cdda103c9da5d38e37d41240a1c2f
jerp86 <jerp4@hotmail.com>
2019-12-11 07:11:29 -0300

    Update do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,20 +8,41 @@ class UserController {
       user = await User.create(req.body);
     }
 
-    const { id, name, phone, email, password_hash } = user;
+    const { id, name, phone, email } = user;
 
     return res.json({
       id,
       name,
       phone,
       email,
-      password_hash,
     });
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, phone } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      phone,
+      email,
+    });
   }
 }
 
