fcdb8f4d06012d3cb619108ad2a51be3ba4e6382
jerp86 <jerp4@hotmail.com>
2019-12-11 07:00:44 -0300

    Middleware de autenticação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -18,6 +18,11 @@ class UserController {
       password_hash,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
