bee4d47fe4f6fcef1a9336aaa63c79fd12d9f735
jerp86 <jerp4@hotmail.com>
2019-12-11 23:51:21 -0300

    Model de Funcionário

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Employees from '../app/models/Employees';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Employees];
 
 class Database {
   constructor() {
